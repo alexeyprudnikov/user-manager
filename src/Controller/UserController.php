00386c0e6c42c6d07c7e0d37c9585d5f8e5b2a54
aprudnikov <a.prudnikov@neusta.de>
2019-10-16 12:39:09 +0200

    weitere entwicklung

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Helper\CreateUserValidator;
+use App\Repository\UserRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,16 @@ class UserController extends AbstractController
 
     /** @var string $error */
     protected $error;
+
+    protected $userFinder;
+
+    protected $userManager;
+
+    public function __construct(UserRepository $userRepository, ObjectManager $objectManager)
+    {
+        $this->userFinder = $userRepository;
+        $this->userManager = $objectManager;
+    }
 
     /**
      * @Route("/create", name="create")
@@ -69,9 +81,7 @@ class UserController extends AbstractController
      */
     protected function findUserByName(string $username)
     {
-        $userRepository = $this->getDoctrine()->getRepository(User::class);
-
-        return $userRepository->findOneBy(['username' => $username]);
+        return $this->userFinder->findOneBy(['username' => $username]);
     }
 
     /**
@@ -80,11 +90,53 @@ class UserController extends AbstractController
      */
     protected function doCreate(string $username, string $password): void
     {
-        $userManager = $this->getDoctrine()->getManager();
-
         $user = (new User())->setUsername($username)
                             ->setPassword(md5($password));
-        $userManager->persist($user);
-        $userManager->flush();
+        $this->userManager->persist($user);
+        $this->userManager->flush();
+    }
+
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(): Response
+    {
+        $users = $this->userFinder->findAll();
+        return $this->render('user/list.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/disable", name="disable", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function disable(Request $request): Response
+    {
+        $id = $request->get('id');
+        $user = $this->userFinder->find($id);
+        if($user === null) {
+            return $this->render('user/message.html.twig', ['message' => 'User with ID: ' . $id . ' does not found.']);
+        }
+        $disabled = $request->get('disabled') !== '1';
+        $user->setDisabled($disabled);
+        $this->userManager->flush();
+        return $this->redirectToRoute('list');
+    }
+
+    /**
+     * @Route("/delete", name="delete", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function delete(Request $request): Response
+    {
+        $id = $request->get('id');
+        $user = $this->userFinder->find($id);
+        if($user === null) {
+            return $this->render('user/message.html.twig', ['message' => 'User with ID: ' . $id . ' does not found.']);
+        }
+        $this->userManager->remove($user);
+        $this->userManager->flush();
+        return $this->redirectToRoute('list');
     }
 }
